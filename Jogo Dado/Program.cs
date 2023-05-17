536576b96eb0d7030dd3b4eccbb06954c9e7e0df
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-17 20:07:58 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo Dado/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Jogo_Dado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int round = 1;
+            int seuNumero, numeroAdversario, score = 0, scoreMaquina = 0;
+
+            while (round <= 10)
+            {
+                Console.WriteLine("Pressione qualquer tecla para rodar o dado.");
+                Console.ReadKey();
+                Random nAleatorio = new Random();
+                seuNumero = nAleatorio.Next(11);
+                Console.WriteLine($"voce tirou o numero: {seuNumero}");
+                Console.WriteLine("...");
+                numeroAdversario = nAleatorio.Next(11);
+                Console.WriteLine($"O adversário tirou o numero: {numeroAdversario}");
+                if (seuNumero > numeroAdversario)
+                {
+                    round++;
+                    score++;
+                    Console.WriteLine("Voce ganhou esse round!");
+                    Console.WriteLine($"O resultado esta: Player: {score}. Adversario: {scoreMaquina}");
+                }
+                else if (seuNumero == numeroAdversario)
+                {
+                    round++;
+                    Console.WriteLine("Empatou");
+                    Console.WriteLine($"O resultado esta: Player: {score}. Adversario: {scoreMaquina}");
+                }
+                else
+                {
+                    round++;
+                    scoreMaquina++;
+                    Console.WriteLine("Voce perdeu este round");
+                    Console.WriteLine($"O resultado esta: Player: {score}. Adversario: {scoreMaquina}");
+                }
+                if (round == 11)
+                {
+                    if (score > scoreMaquina)
+                    {
+                        Console.WriteLine("Voce ganhou GG");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Voce perdeu!");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
